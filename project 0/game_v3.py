8b1f23682a6e4b49f26f8e19584f14ecf3958dbc
niceonepv <niceone.pv@gmail.com>
2023-04-21 15:59:57 +0300

    Fix infinite cycle

--- project 0/game_v3.py
+++ project 0/game_v3.py
@@ -6,8 +6,9 @@
 import numpy as np
 
 
-def smart_predict(number, left : int = 0, right: int = 100) -> int:
-    """Начинаем с середины и действуем методом деления пополам, узнавая с какой стороны число от текущего
+def smart_predict(number, left : int = 0, right: int = 101) -> int:
+    """Умный поиск
+    Начинаем с середины и действуем методом деления пополам, узнавая с какой стороны число от текущего
 
     Args:
         predict_number (int, optional): Предполагаемое число. Defaults to 50.
@@ -16,7 +17,7 @@ def smart_predict(number, left : int = 0, right: int = 100) -> int:
         int: Число попыток
     """
     count = 0
-    while right-left >=1:
+    while right != left:
         predict_num = (right + left) // 2
         
         if predict_num > number:
@@ -35,14 +36,14 @@ def score_game(smart_predict) -> int:
     """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
 
     Args:
-        random_predict ([type]): функция угадывания
+        smart_predict ([type]): функция угадывания
 
     Returns:
         int: среднее количество попыток
     """
     count_ls = []
     #np.random.seed(1)  # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(200))  # загадали список чисел
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
 
     for number in random_array:
         count_ls.append(smart_predict(number))
