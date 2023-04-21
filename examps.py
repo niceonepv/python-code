db01a1e8ebcd0a9a7cc1c0523eadf723d33fea97
niceonepv <niceone.pv@gmail.com>
2023-04-21 03:12:22 +0300

    yyyy

--- examps.py
+++ examps.py
@@ -9,7 +9,7 @@ logging.basicConfig(
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to 1me!")
 
 
 if __name__ == '__main__':
