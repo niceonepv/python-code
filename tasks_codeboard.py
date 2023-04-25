11a9c9dad27dc2dfcd4fe5b07a8ca2c02657010c
niceonepv <niceone.pv@gmail.com>
2023-04-25 12:19:27 +0300

    update .gitignore

--- tasks_codeboard.py
+++ tasks_codeboard.py
@@ -1,18 +1,1 @@
-from collections import defaultdict, deque
-
-def task_manager (task_list: list) -> dict:
-    result = defaultdict(deque)
-    for task_num, serv_name, importance in task_list:
-        if importance == True:
-            result[serv_name].appendleft(task_num)
-        if importance == False:
-            result[serv_name].append(task_num)
-    return result
-                  
-tasks = [(36871, 'office', False),
-(40690, 'office', False),
-(35364, 'voltage', False),
-(41667, 'voltage', True),
-(33850, 'office', False)]
-
-print(task_manager(tasks))
+import nympy as np
